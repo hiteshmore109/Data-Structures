abf3b3e924df5cf24d1d4bc6e8229702e6ba169c
“Hitesh <hitesh.omprakashmore@gmail.com>
2023-03-21 07:10:32 +0530

    Updated the program to take the input of vertices and the edges connecting them from the users

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -52,40 +52,50 @@ int Cqueue::peek() const{
 }
 
 int main(){
+    int n, start, edges, u, v;
+    cout<<"Enter the number of vertices: ";
+    cin>>n;
     //queue to keep track of vertex yet to be explored
-    Cqueue exploration(7);
+    Cqueue explorationq(n);
     //to keep track whether the vertex is visited or not
-    int visited[7] = {0, 0, 0, 0, 0, 0, 0};
-    //adjacency list for the graph
-    //for visual graph refer to site: https://www.researchgate.net/profile/Clive-Elphick-2/publication/236834786/figure/fig1/AS:619619178016768@1524740132217/Graph-on-7-vertices-with-degree-sequence-4-4-4-3-3-3-3-ph-2-m-3503.png
-    int adjacencyl[7][7] = {
-        {0, 1, 1, 1, 1, 0, 0},
-        {1, 0, 1, 1, 0, 1, 0},
-        {1, 1, 0, 0, 1, 0, 1},
-        {1, 1, 0, 0, 0, 1, 0},
-        {1, 0, 1, 0, 0, 0, 1},
-        {0, 1, 0, 1, 0, 0, 1},
-        {0, 0, 1, 0, 1, 1, 0}
-    };
-    //starting from the 6 vertex(index 5 in list)
-    int i = 5;
-    //marking vertex 5 as visited
-    visited[i] = 1;
-    exploration.enqueue(i);
-    cout<<i<<" ";
-    while(!exploration.isEmpty()){
+    int visited[n];
+    for(int i = 0; i < n; i ++)
+        visited[i] = 0;
+    //adjacency matrix for the graph
+    int adjacencym[n][n];
+    for(int i = 0; i < n; i ++)
+        for(int j = 0; j < n; j++)
+            adjacencym[i][j] = 0;
+    //number of edges present in the graph if undirected double the number
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+    //input edges ex: if 1 connected to 2 then input: "1 2"
+    for (int i = 0; i < edges; i++) {
+        cout << "Enter the edges (u v): ";
+        cin >> u >> v;
+        adjacencym[u-1][v-1] = 1;
+        // for undirected graph uncomment the next line and also half the number of edges since the connection will be made between 2 using the next line
+        // adjacencym[v-1][u-1] = 1; 
+    }
+    cout<<"\nEnter the element from which breadth first search starts: ";
+    cin>>start;
+    cout<<start<<" ";
+    start--;
+    visited[start] = 1;
+    explorationq.enqueue(start);
+    while(!explorationq.isEmpty()){
         //take the first element from the queue for exploration
-        int n = exploration.peek();
-        exploration.dequeue();
-        for(int j = 0; j < 7; j++){
-            //if the vertices are connected with each other is visiting the vertex n for first time
-            if(adjacencyl[n][j] == 1 && visited[j] == 0){
+        int node = explorationq.peek();
+        explorationq.dequeue();
+        for(int j = 0; j < n; j++){
+            //if the vertices are connected with each other and is visiting the vertex for first time
+            if(adjacencym[node][j] == 1 && visited[j] == 0){
                 //then print its connected vertices
-                cout<<j<<" ";
+                cout<<j+1<<" ";
                 //mark visited 
                 visited[j] = 1;
                 //add the vertex for exploration
-                exploration.enqueue(j);
+                explorationq.enqueue(j);
             }
         }
     }
